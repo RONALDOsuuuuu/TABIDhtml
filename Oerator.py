10c7fc9bf5183b258ec07da49d23c5b59cf647c0
RONALDOsuuuuu <zahir.tahi.tabi@gmail.com>
2026-09-05 15:59:08 +0600

    Add files via upload

---
+++ Oerator.py
@@ -0,0 +1,11 @@
+num1 = 45
+num2 = 13
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition:",num1+num2)
+print("Diffrence :",num1-num2)
+print("Product:",num1*num2)
+print("Divition:",num1/num2)
+print("Flore Divition:",num1//num2)
+print("Modulus Operater :",num1%num2)
+print("Square :", num2**2)
