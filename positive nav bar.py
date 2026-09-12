d58808631e0a9341216216c5fae90560398b0f83
RONALDOsuuuuu <zahir.tahi.tabi@gmail.com>
2026-09-12 15:17:40 +0600

    Add files via upload

---
+++ positive nav bar.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Number to Check"))
+print("Number to be checked:",number)
+if number>0:
+    print("This is a positive number")
+elif number<0:
+    print("This is a nagitive")
+else: 
+    print("This is neutral")
